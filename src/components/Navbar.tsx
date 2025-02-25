628bb9204879b8db958517a651d261b0a7134170
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-02-24 21:17:07 -0600

    HeroSection & FeaturesSecion done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { NAVBAR_HEIGHT } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
@@ -34,6 +35,27 @@ const Navbar = () => {
             </div>
           </Link>
         </div>
+          <p className="text-primary-200 hidden md:block">
+            Discover your perfect rental apartment with our advanced search
+          </p>
+          <div className="flex items-center gap-5">
+            <Link href='/signin'>
+              <Button 
+                variant='outline'
+                className="text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg"
+              >
+                Sign In
+                </Button>
+            </Link>
+            <Link href='/signin'>
+              <Button 
+                variant='secondary'
+                className="text-white border-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg"
+              >
+                Sign Up
+                </Button>
+            </Link>
+          </div>
       </div>
     </div>
   );
